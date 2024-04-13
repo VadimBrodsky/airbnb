38af588e677b3a68020f177fed870eaffbbe1438
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-04-13 15:49:10 -0400

    Complete the layout of the signin modal

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,13 +1,58 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { useWarmUpBrowser } from "@/hoooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Login: React.FC = () => {
   useWarmUpBrowser();
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorLine}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={defaultStyles.btnOutline}>
+          <Ionicons name="call-outline" size={24} style={defaultStyles.btnIcon} />
+          <Text style={defaultStyles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={defaultStyles.btnOutline}>
+          <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+          <Text style={defaultStyles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={defaultStyles.btnOutline}>
+          <Ionicons name="logo-google" size={24} style={defaultStyles.btnIcon} />
+          <Text style={defaultStyles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={defaultStyles.btnOutline}>
+          <Ionicons name="logo-facebook" size={24} style={defaultStyles.btnIcon} />
+          <Text style={defaultStyles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,9 +60,19 @@ const Login: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
-  }
-})
+  },
+  separatorLine: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: "mon-sb",
+    color: Colors.grey,
+  },
+});
 
 export default Login;
