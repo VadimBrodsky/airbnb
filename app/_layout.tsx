d9ea7c71497fbdf1b2f0ed17eb12b0ed137a61e3
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-04-24 18:26:46 -0400

    Start on Listing detail page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -98,6 +98,7 @@ function RootLayoutNav() {
         name="listing/[id]"
         options={{
           headerTitle: "",
+          headerTransparent: true,
         }}
       />
       <Stack.Screen
--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,16 +1,35 @@
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import listingsData from "@/assets/data/airbnb-listings.json";
+import Colors from "@/constants/Colors";
+import Animated from "react-native-reanimated";
 
-// import { Container } from './styles';
+const IMG_HEIGHT = 300;
+const { width } = Dimensions.get("window");
 
 const Listing: React.FC = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const listing = (listingsData as any[]).find((item) => item.id === id);
+
   return (
-    <View>
-      <Text>{id}</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView>
+        <Animated.Image source={{ uri: listing.xl_picture_url }} style={styles.image} />
+      </Animated.ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  image: {
+    height: IMG_HEIGHT,
+    width,
+  },
+});
 
 export default Listing;
