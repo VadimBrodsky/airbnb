1cdea4f9a0731fd1efae83ce85a5d4948394e864
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-04-25 06:52:35 -0400

    Implement the parallax header image

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,10 +1,17 @@
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from "react-native";
 import listingsData from "@/assets/data/airbnb-listings.json";
 import Colors from "@/constants/Colors";
-import Animated from "react-native-reanimated";
+import Animated, {
+  SlideInDown,
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
 import { Ionicons } from "@expo/vector-icons";
+import { defaultStyles } from "@/constants/Styles";
 
 const IMG_HEIGHT = 300;
 const { width } = Dimensions.get("window");
@@ -12,11 +19,38 @@ const { width } = Dimensions.get("window");
 const Listing: React.FC = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
   const listing = (listingsData as any[]).find((item) => item.id === id);
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+
+  const scrollOffset = useScrollViewOffset(scrollRef);
+
+  const imageAnimatesStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+          ),
+        },
+        {
+          scale: interpolate(scrollOffset.value, [-IMG_HEIGHT, 0, IMG_HEIGHT], [2, 1, 1]),
+        },
+      ],
+    };
+  });
 
   return (
     <View style={styles.container}>
-      <Animated.ScrollView>
-        <Animated.Image source={{ uri: listing.xl_picture_url }} style={styles.image} />
+      <Animated.ScrollView
+        ref={scrollRef}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        scrollEventThrottle={16}
+      >
+        <Animated.Image
+          source={{ uri: listing.xl_picture_url }}
+          style={[styles.image, imageAnimatesStyle]}
+        />
 
         <View style={styles.infoContainer}>
           <Text style={styles.name}>{listing.name}</Text>
@@ -49,6 +83,21 @@ const Listing: React.FC = () => {
           <Text style={styles.description}>{listing.description}</Text>
         </View>
       </Animated.ScrollView>
+
+      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+        <View
+          style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}
+        >
+          <TouchableOpacity style={styles.footerText}>
+            <Text style={styles.footerPrice}>${listing.price}</Text>
+            <Text>night</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[defaultStyles.btn, { paddingHorizontal: 20 }]}>
+            <Text style={defaultStyles.btnText}>Reserve</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </View>
   );
 };
