bdde90abdc6d59b4aa02060f6075e8613e22cd15
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-05-01 19:05:49 -0400

    Add profile editing

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,23 +1,145 @@
-import { useAuth } from "@clerk/clerk-expo";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { SignedOut, useAuth, useUser } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, StyleSheet, Image, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // import { Container } from './styles';
 
 const Profile: React.FC = () => {
   let { signOut, isSignedIn } = useAuth();
+  let { user } = useUser();
+  let [firstName, setFirstName] = useState(user?.firstName);
+  let [lastName, setLastName] = useState(user?.lastName);
+  let [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  let [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEmail(user.emailAddresses[0].emailAddress);
+  }, [user]);
+
+  let onSaveUser = async () => {
+    try {
+      if (!firstName || !lastName) {
+        return;
+      }
+
+      await user?.update({ firstName, lastName });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setEdit(false);
+    }
+  };
+
+  let onCaptureImage = async () => {};
 
   return (
-    <View>
-      <Button title="Log out" onPress={() => signOut()} />
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name="notifications-outline" size={26} />
+      </View>
+
+      {user && (
+        <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row", gap: 6 }}>
+            {edit ? (
+              <View style={styles.editRow}>
+                <TextInput
+                  placeholder="First name"
+                  value={firstName || ""}
+                  onChangeText={setFirstName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TextInput
+                  placeholder="Last name"
+                  value={lastName || ""}
+                  onChangeText={setLastName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TouchableOpacity onPress={onSaveUser}>
+                  <Ionicons name="checkmark-outline" size={24} color={Colors.dark} />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={styles.editRow}>
+                <Text style={{ fontFamily: "mon-b", fontSize: 22 }}>
+                  {firstName} {lastName}
+                </Text>
+                <TouchableOpacity onPress={() => setEdit(true)}>
+                  <Ionicons name="create-outline" size={24} color={Colors.dark} />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+          <Text>{email}</Text>
+          <Text>{user?.createdAt?.toLocaleDateString()}</Text>
+        </View>
+      )}
+
+      {isSignedIn && <Button title="Log out" onPress={() => SignedOut()} color={Colors.dark} />}
+
       {!isSignedIn && (
-        <Link href={"/(modals)/login"}>
-          <Text>Log in</Text>
+        <Link href={"/(modals)/login"} asChild>
+          <Button title="Log In" color={Colors.dark} />
         </Link>
       )}
-    </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    padding: 24,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    fontFamily: "mon-sb",
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: Colors.white,
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 23,
+    elevation: 2,
+    shadowColor: Colors.black,
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: { width: 1, height: 2 },
+    alignItems: "center",
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    height: 50,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+});
 
 export default Profile;
